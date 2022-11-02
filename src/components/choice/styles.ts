c92a3869fafa3575646857a09e844992fda61bf1
Jirlan Souza <jirlansouza08@gmail.com>
2022-11-01 21:27:33 -0400

    Creted the Choice component

---
+++ src/components/choice/styles.ts
@@ -0,0 +1,64 @@
+import { styled } from "@stitches/react";
+import { theme } from "../../styles/theme";
+
+export const Container = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: 200,
+  height: 220,
+});
+
+export const Title = styled("h6", {
+  fontSize: 22,
+  fontWeight: 600,
+  letterSpacing: 1,
+  textTransform: "uppercase",
+  marginBottom: 40,
+  color: theme.colors.textPrimary,
+});
+
+export const NotChoiceSelectd = styled("span", {
+  width: 160,
+  height: 160,
+  borderRadius: "50%",
+  backgroundColor: "rgba(3,3,3, 0.1)",
+  boxShadow: "0 0 2px 5px rgba(3,3,3, 0.1)",
+  zIndex: 1,
+});
+
+export const WinnerEffectConatiner = styled("span", {
+  position: "relative",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: 160,
+  height: 160,
+});
+
+export const WinnerEffect = styled("span", {
+  position: "absolute",
+  width: "inherit",
+  height: "inherit",
+  display: "flex",
+  background: "transparent",
+  borderRadius: "50%",
+  zIndex: 0,
+
+  variants: {
+    scale: {
+      1: {
+        transform: "scale(1.5)",
+        background: "rgba(255,255,255,0.05)",
+      },
+      2: {
+        transform: "scale(2.1)",
+        background: "rgba(255,255,255,0.04)",
+      },
+      3: {
+        transform: "scale(2.7)",
+        background: "rgba(255,255,255,0.01)",
+      },
+    },
+  },
+});
