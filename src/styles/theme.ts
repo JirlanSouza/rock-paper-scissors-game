c84bc929890b9783bb318d6be6c7e22dec489443
Jirlan Souza <jirlansouza08@gmail.com>
2022-11-01 21:28:01 -0400

    Change the theme, update textPrimary color

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,7 @@ import { createTheme } from "@stitches/react";
 
 export const theme = createTheme({
   colors: {
-    textPrimary: "#FAFFFFAC",
+    textPrimary: "#FAFAFF",
     textDark: "hsl(229, 25%, 31%)",
     textScore: "hsl(229, 64%, 46%)",
     textLose: "#DA1F62",
